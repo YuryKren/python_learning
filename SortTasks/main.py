96765e67d0cfba5b8e30b5f133ab7aaa308e6ce9
Yury Kren <yuryhumanoid@outlook.com>
2023-09-06 18:34:14 +0300

    version 0.6

--- SortTasks/main.py
+++ SortTasks/main.py
@@ -1,18 +1,20 @@
-array = [8, -4, 9, 1, 5]
+array = [8, -4, 9, 1, 5, 3, 7, 2, -3, 4]
+
+mem_value = array[0]
 
 for i, num in enumerate(array):
-    mem_value = 0
     if i == len(array) - 2:
         break
     elif num > array[i+1]:
         mem_value = array[i+1]
         array[i+1] = num
-        for j in range(i, -1, -1):
+        if i == 0:
+            array[i] = mem_value
+        for j in range(i, 0, -1):
             if array[j-1] > mem_value:
-                array[j] = array[j+1]
+                array[j] = array[j-1]
             else:
                 array[j] = mem_value
-
-
+                break
 
 print(array)
