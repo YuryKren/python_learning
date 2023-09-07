ea5ab4eef8d263cc2106012871e4590fe6b9e163
YuryKren <yuryhumanoid@outlook.com>
2023-09-07 11:37:17 +0300

    merge sorted lists version 0.5

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-# online sorting
-array_1 = [2, 6, 9, 12, 15]
-array_2 = [1, 4, 5, 11, 17]
+# merge sorted lists
+
+array = []
+array_1 = [2, 4, 7, 9, 12, 15, 16, 21]
+array_2 = [1, 4, 5, 11, 17, 18, 20]
 
 print(array_1, f'\n{array_2}')
 
+if len(array_1) >= len(array_2):
+    lght = len(array_2)
+else:
+    lght = len(array_1)
+
+
+for i in range(lght):
+    if array_1[i] <= array_2[i]:
+        array.append(array_1[i])
+        array.append(array_2[i])
+    else:
+        array.append(array_2[i])
+        array.append(array_1[i])
+else:
+    if len(array_1) > len(array_2):
+        array.extend(array_1[i+1:])
+    elif len(array_1) < len(array_2):
+        array.extend(array_2[i+1:])
+
+print(array)
