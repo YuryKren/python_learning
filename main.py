207198c57dd60af23689fda6a98e56fd237710f8
Yury Kren <yuryhumanoid@outlook.com>
2023-09-13 23:53:31 +0300

    result

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ i = 0
 j = 0
 
 while i < len(array_1) and j < len(array_2):
-    if array_1[i] >= array_2[j]:
+    if array_1[i] < array_2[j]:
         array.append(array_1[i])
         i += 1
     else:
         array.append(array_2[j])
         j += 1
 
-array.append(array_1[i:])
-array.append(array_2[j:])
+array.extend(array_1[i:])
+array.extend(array_2[j:])
 
 print(array)
